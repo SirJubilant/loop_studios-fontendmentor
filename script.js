bc8152f78da7bcac9b154c8e25e372b7e712e78f
Jubilant Agida <jubilantagida@gmail.com>
2022-10-01 00:04:24 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+let menu = document.querySelector('.hamburger');
+let close = document.querySelector('.close');
+let navlist = document.querySelector('.nav-list');
+
+
+
+
+// menu.addEventListener('click', () => {
+//     if(close.contains('close')) {
+//         close.classList.add('menu-active');
+//     }
+// })
+
+menu.addEventListener("click", ()=> {
+    navlist.classList.toggle('active');
+
+    if (close.classList.contains('close')) {
+        close.classList.toggle('menu-active')
+        menu.classList.remove('menu-active')
+    }
+});
+
+close.addEventListener("click", ()=> {
+    navlist.classList.toggle('active');
+
+
+    menu.classList.add('menu-active');
+    close.classList.remove('menu-active');
+});
+
+
+
+window.onscroll = () => {
+    navlist.classList.remove('active');
+};
+
